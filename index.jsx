46a77c1f3dcb9380818013cc73d79113be32bdf7
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-11-04 12:04:58 -0500

    Netlify was not getting the images for some reason.

--- index.jsx
+++ index.jsx
@@ -31,7 +31,7 @@ function App() {
       startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "./images/Mount-Fuji.png"
+        imageUrl: "https://github.com/alexander-kazanski/travel-log/blob/main/images/Mount-Fuji.png?raw=true"
       },
       {
         title: "Sydney Opera House",
@@ -40,7 +40,7 @@ function App() {
         startDate: "27 May, 2023",
         endDate: "8 Jun, 2023",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        imageUrl: "./images/Sydney-Opera-House.png"
+        imageUrl: "https://github.com/alexander-kazanski/travel-log/blob/main/images/Sydney-Opera-House.png?raw=true"
       },
       {
         title: "Geirangerfjord",
@@ -49,7 +49,7 @@ function App() {
         startDate: "01 Oct, 2024",
         endDate: "18 Nov, 2024",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "./images/Geirangerfjord.png"
+        imageUrl: "https://github.com/alexander-kazanski/travel-log/blob/main/images/Geirangerfjord.png?raw=true"
       }
     ]
   
